a31e90fcce72449e8c0159f35230002123cb7055
iulian.redinciuc <iredinciuc.com>
2018-08-15 23:14:37 +0300

    added get, post and delete

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,39 +1,85 @@
 (function initIIFE() {
-    let petsContainer = document.getElementById("petsContainer");
-    
-    petsContainer.addEventListener("click", function onClick(e) {
-        if(e.target.tagName === "BUTTON") {
-            switch(e.target.dataset.type) {
-                case "edit":
-                    swal({
-                        title: "Edit pet", 
-                        html: createPetCard({
-                            name: "Bob",
-                            type: "cat",
-                            created: Date.now()
-                        }, true),  
-                        confirmButtonText: "Save",
-                        showCancelButton: true,
-                        cancelButtonText: "Cancel",
-                        width: "50%" 
-                    });
-                    break;
-                case "delete":
-                    break;
-                default:
-                    break;
-            }
-        }
+  let petsContainer = document.getElementById("petsContainer");
+
+  petsContainer.addEventListener("click", function onClick(e) {
+    if (e.target.tagName === "BUTTON") {
+      switch (e.target.dataset.type) {
+        case "edit":
+          swal({
+            title: "Edit pet",
+            html: createPetCard(
+              {
+                name: "Bob",
+                type: "cat",
+                created: Date.now()
+              },
+              true
+            ),
+            confirmButtonText: "Save",
+            showCancelButton: true,
+            cancelButtonText: "Cancel",
+            width: "50%"
+          });
+          break;
+        case "delete":
+          break;
+        default:
+          break;
+      }
+    }
+  });
+
+  /* get pets */
+  let getPet = new XMLHttpRequest();
+  getPet.open("GET", `/pets`);
+  getPet.setRequestHeader("Content-Type", "application/json");
+  getPet.addEventListener("load", function onLoad() {
+    switch (getPet.status) {
+      case 200:
+        let pets = JSON.parse(getPet.response);
+        pets.forEach(pet => {
+          petEl = createPetCard(pet);
+          petEl.setAttribute("data-id", pet.id);
+
+          petsContainer.appendChild(petEl);
+          /* delete pet */
+          DataService.deletePet(petEl, pet.id);
+        });
+    }
+  });
+
+  getPet.addEventListener("error", function onError() {});
+
+  getPet.send();
+
+  /* create pet */
+  document.getElementById("sbm-form").addEventListener("click", e => {
+    e.preventDefault();
+    let formData = document.getElementById("createForm").elements;
+    let payload = {
+      name: formData[0].value,
+      type: formData[1].value
+    };
+
+    let createPet = new XMLHttpRequest();
+
+    createPet.open("POST", "/pets");
+
+    createPet.setRequestHeader("Content-Type", "application/json");
+
+    createPet.addEventListener("load", function onLoad() {
+      switch (createPet.status) {
+        case 200:
+          petData = JSON.parse(createPet.response);
+          petEl = createPetCard(petData);
+          petEl.setAttribute("data-id", petData.id);
+          petsContainer.appendChild(petEl);
+          DataService.deletePet(petEl, petData.id);
+      }
     });
 
-    petsContainer.appendChild(createPetCard({
-        name: "Dummy 1",
-        type: "cat",
-        created: Date.now()
-    }));
-    petsContainer.appendChild(createPetCard({
-        name: "Dummy 2",
-        type: "dog",
-        created: Date.now()
-    }));
-}());
+    createPet.addEventListener("error", function onError() {});
+
+    createPet.send(JSON.stringify(payload));
+  });
+})();
--- public/javascript/utils.js
+++ public/javascript/utils.js
@@ -1,53 +1,73 @@
 /**
- * 
- * @param {{name: string, type: string, created: number}} param0 
+ *
+ * @param {{name: string, type: string, created: number}} param0
  */
-function createPetCard({name, type, created}, isEditable = false) {
-    let card = document.createElement("div");
-    card.classList.add("pet", `pet--${type}`);
+function createPetCard({ name, type, created }, isEditable = false) {
+  let card = document.createElement("div");
+  card.classList.add("pet", `pet--${type}`);
 
-    let img = document.createElement("img");
-    img.width = 64;
-    img.src = `public/images/${type}.jpg`;
-    img.alt = `${type} image`;
+  let img = document.createElement("img");
+  img.width = 64;
+  img.src = `public/images/${type}.jpg`;
+  img.alt = `${type} image`;
 
-    let nameEl = document.createElement("h1");
-    nameEl.contentEditable = isEditable;
-    nameEl.innerText = name;
+  let nameEl = document.createElement("h1");
+  nameEl.contentEditable = isEditable;
+  nameEl.innerText = name;
 
-    let ageEl = document.createElement("p");
-    let days = document.createElement("span");
-    days.contentEditable = isEditable;
-    days.innerText = getAge(created);
+  let ageEl = document.createElement("p");
+  let days = document.createElement("span");
+  days.contentEditable = isEditable;
+  days.innerText = getAge(created);
 
-    ageEl.classList.add("age");
-    ageEl.appendChild(document.createElement("span"));
-    ageEl.appendChild(days);
+  ageEl.classList.add("age");
+  ageEl.appendChild(document.createElement("span"));
+  ageEl.appendChild(days);
 
-    let editBtn = document.createElement("button");
-    editBtn.classList.add("btn", "edit-btn");
-    editBtn.setAttribute("data-type", "edit");
-    editBtn.innerText = "Edit";
+  let editBtn = document.createElement("button");
+  editBtn.classList.add("btn", "edit-btn");
+  editBtn.setAttribute("data-type", "edit");
+  editBtn.innerText = "Edit";
 
-    let deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("btn", "delete-btn");
-    deleteBtn.setAttribute("data-type", "delete");
-    deleteBtn.innerText = "Delete";
+  let deleteBtn = document.createElement("button");
+  deleteBtn.classList.add("btn", "delete-btn");
+  deleteBtn.setAttribute("data-type", "delete");
+  deleteBtn.innerText = "Delete";
 
-    card.appendChild(img);
-    card.appendChild(nameEl);
-    card.appendChild(ageEl);
-    if(!isEditable) {
-        card.appendChild(editBtn);
-        card.appendChild(deleteBtn);
-    }
+  card.appendChild(img);
+  card.appendChild(nameEl);
+  card.appendChild(ageEl);
+  if (!isEditable) {
+    card.appendChild(editBtn);
+    card.appendChild(deleteBtn);
+  }
 
-    return card;
+  return card;
 }
 
 function getAge(timestamp) {
-    let age = Date.now() - timestamp;
-    let days = Math.floor(age / (1000 * 60 * 60 * 24));
+  let age = Date.now() - timestamp;
+  let days = Math.floor(age / (1000 * 60 * 60 * 24));
 
-    return days;
+  return days;
+}
+
+class DataService {
+
+  static deletePet(petEl, petID) {
+    petEl.querySelector("[data-type=delete]").addEventListener("click", () => {
+
+      let del = new XMLHttpRequest();
+      del.open("DELETE", `/pets/${petID}`);
+      del.setRequestHeader("Content-Type", "application/json");
+      del.addEventListener("load", function onLoad() {
+        document.querySelector(`[data-id='${petID}']`).remove();
+      });
+
+      del.addEventListener("error", function onError() {});
+
+      del.send();
+    });
+  }
+
 }
