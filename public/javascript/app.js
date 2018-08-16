b8f7dc94da9d4a4a3a667d7e91ac2cc24e13b55c
iulian.redinciuc <iredinciuc.com>
2018-08-16 20:05:10 +0300

    added search optimization

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -82,4 +82,74 @@
 
     createPet.send(JSON.stringify(payload));
   });
+
+  /* search pet */
+  document.getElementById("searchForm").addEventListener("submit", e => {
+    e.preventDefault();
+
+    let formData = document.getElementById("searchForm").elements;
+    let name = formData[0].value;
+    let search = new XMLHttpRequest();
+    search.open("GET", `/pets/?name=${name}`);
+    search.setRequestHeader("Content-Type", "application/json");
+    search.addEventListener("load", function onLoad() {
+      switch (search.status) {
+        case 200:
+        petsContainer.innerHTML = null;
+          let pets = JSON.parse(search.response);
+          pets.forEach(pet => {
+            petEl = createPetCard(pet);
+            petEl.setAttribute("data-id", pet.id);
+  
+            petsContainer.appendChild(petEl);
+            /* delete pet */
+            DataService.deletePet(petEl, pet.id);
+          });
+      }
+    });
+  
+    search.addEventListener("error", function onError() {});
+  
+    search.send();
+  });
+  timeout = undefined
+  document.getElementById("searchForm").addEventListener("keyup", () => {
+    
+    
+
+      timeout =  setTimeout(function() {
+           
+        let formData = document.getElementById("searchForm").elements;
+        let name = formData[0].value;
+        let search = new XMLHttpRequest();
+        search.open("GET", `/pets/?name=${name}`);
+        search.setRequestHeader("Content-Type", "application/json");
+        search.addEventListener("load", function onLoad() {
+          switch (search.status) {
+            case 200:
+            petsContainer.innerHTML = null;
+              let pets = JSON.parse(search.response);
+              pets.forEach(pet => {
+                petEl = createPetCard(pet);
+                petEl.setAttribute("data-id", pet.id);
+      
+                petsContainer.appendChild(petEl);
+                /* delete pet */
+                DataService.deletePet(petEl, pet.id);
+              });
+          }
+        });
+      
+        search.addEventListener("error", function onError() {});
+      
+        search.send();
+
+    },2000)
+  })
+
+  document.getElementById("searchForm").addEventListener("keydown", function(){
+    clearTimeout(timeout);
+  })
+
+
 })();
